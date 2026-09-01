99910ec0d304f38d0f065a1134b8b546d70ad36e
Paulo Salinas <p.salinaslueyza@gmail.com>
2026-09-01 00:15:59 -0400

    ultimos cambios

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // Cakekulator - Service Worker para Soporte Offline & Push
 // ==========================================
 
-const CACHE_NAME = 'cakekulator-v3.9';
+const CACHE_NAME = 'cakekulator-v4.0';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
